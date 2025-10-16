e5378888f4fb31cdd41e04fbc823e750af603953
Hugo Anderzon <huan2300@student.miun.se>
2025-10-16 10:17:18 +0200

    Dinosaurcontroller test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,7 @@ module com.dinosaur.dinosaurexploder {
     requires annotations;
     requires javafx.base;
     requires com.almasb.fxgl.entity;
+   
 
     opens assets.textures;
     opens assets.sounds;
